7a4b96ed46ff89c4d6a8d5f8aeda4262bdaf1f3f
ruslankhh <ruslankhh@yandex-team.ru>
2018-09-16 23:46:29 +0300

    fix: edit layout

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -19,15 +19,11 @@ export const ProjectPageTemplate = ({
     <section className="section">
       {helmet || ''}
       <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-          </div>
-        </div>
+        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+          {title}
+        </h1>
+        <p>{description}</p>
+        <PostContent content={content} />
       </div>
     </section>
   )
